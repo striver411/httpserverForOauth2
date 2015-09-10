b2aed2ebed784aff7a11caba3a239bf2a79bf398
plutoshe <plutoshe@gmail.com>
2015-09-10 18:04:48 +0800

    router: Implement exclusive request for Deepshare API
    Based on the DeepShare API design doc[https://docs.google.com/document
    /d/1ZHuiWy5FBRBqbxUrNYuPOeHOp2AxHgdjvS57j_4pNfs/edit], we implement
    correpsonding api request in externelRequest.go.
    
    Besides, this commit moves the handle about the main view into
    renderView.go to make the render system more clean.

---
+++ render/externelRequest.go
@@ -0,0 +1,110 @@
+package render
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+func checkSession(w *http.ResponseWriter, r *http.Request) string {
+	session, err := store.Get(r, "session-name")
+	fmt.Println(session)
+	if err != nil {
+		// http.Error(w, err.Error(), 500)
+		http.Redirect(w, r, "/redirect", http.StatusUnauthorized)
+		return
+	}
+	fmt.Println(session)
+	_, ok := session.Values["AccountID"]
+	if !ok {
+		http.Redirect(w, r, "/redirect", http.StatusUnauthorized)
+		return
+	}
+	str, ok := session.Values["AccountID"].(string)
+
+	if !ok {
+		http.Redirect(w, r, "/redirect", http.StatusUnauthorized)
+		return
+	}
+	return str
+}
+
+func requestApplist(accountID string) ([]AppListInfo, error) {
+	u, _ := url.Parse("http://deepshare.chinacloudapp.cn:8080/apps/" + accountID)
+	q := u.Query()
+	u.RawQuery = q.Encode()
+	res, err := http.Get(u.String())
+	fmt.Println(res)
+	if err != nil {
+		return []AppListInfo{}, err
+	}
+
+	// receive data
+	result, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		return []AppListInfo{}, err
+	}
+
+	// covert data to pointed sturct
+	var appList []AppListInfo
+	err = json.Unmarshal(result, &appList)
+	return appList, nil
+}
+
+func requestAppInfo(appID string) (AppStats, error) {
+	u, _ := url.Parse("http://deepshare.chinacloudapp.cn:8080/appstats/" + appID)
+	q := u.Query()
+	u.RawQuery = q.Encode()
+
+	res, err := http.Get(u.String())
+	if err != nil {
+		return AppStats{}, err
+	}
+	if res.StatusCode != 200 {
+		return AppIdentifer{}, fmt.Errorf("Failed request, error Msg = %v", res.Status)
+	}
+	// receive data
+	result, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		return AppStats{}, err
+	}
+
+	// covert data to pointed sturct
+	var appInfo AppStats
+	err = json.Unmarshal(result, &appInfo)
+	return appInfo, nil
+}
+
+func requestRegisterApp(appID string, appInfo PostRegisterAppObj) (AppIdentifer, error) {
+	u, _ := url.Parse("http://deepshare.chinacloudapp.cn:8080/apps/" + appID)
+	q := u.Query()
+	u.RawQuery = q.Encode()
+	jsonStr, err := json.Marshal(appInfo)
+	res, err := http.Post(u.String(), "application/json", bytes.NewBuffer(jsonStr))
+	if err != nil {
+		return AppIdentifer{}, err
+	}
+	if res.StatusCode != 200 {
+		return AppIdentifer{}, fmt.Errorf("Failed request, error Msg = %v", res.Status)
+	}
+	// receive data
+	result, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		return AppIdentifer{}, err
+	}
+
+	fmt.Println(res)
+	fmt.Println(string(result))
+
+	// covert data to pointed sturct
+	var appIden AppIdentifer
+	err = json.Unmarshal(result, &appIden)
+	return appIden, nil
+
+}
--- render/renderView.go
+++ render/renderView.go
@@ -1,44 +1,49 @@
 package render
 
-import (
-	"fmt"
-	"net/http"
-)
+import "net/http"
 
-func AppInfoView(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.Cookies())
-
-	session, err := store.Get(r, "session-name")
-	fmt.Println(session)
-	if err != nil {
-		// http.Error(w, err.Error(), 500)
-		http.Redirect(w, r, "/redirect", http.StatusUnauthorized)
-		return
-	}
-	fmt.Println(session)
-	_, ok := session.Values["UserName"]
-	if !ok {
-		http.Redirect(w, r, "/redirect", http.StatusUnauthorized)
-		return
-	}
-	str, ok := session.Values["UserName"].(string)
-
-	if !ok {
-		http.Redirect(w, r, "/redirect", http.StatusUnauthorized)
-		return
-	}
-
-	str1, _ := session.Values["Token"].(string)
-
-	fmt.Println(str1)
-	userFromToken, err := checkToken(str1)
-	if err != nil {
-		fmt.Println(err)
-	}
-	w.Write([]byte("<html><body>You are in </br> " + str + "</br>" + userFromToken + "</br> URL = " + r.URL.String() + "</body></html>"))
+type AppListInfo struct {
+	AppID   string `json:"appid"`
+	AppName string `json:"appname"`
 }
 
+type AppStats struct {
+	Data []int `json:"data"`
+}
+
+type AppStatsRequst struct {
+	Granularity string //week, day, hour
+	Limit       int
+}
+
+type AppIdentifer struct {
+	AppID string `json:"appid"`
+}
+
+type PostRegisterAppObj struct {
+	AppName     string `json:"name"`
+	FullPkgName string `json:"pkg_name"`
+	UrlIos      string `json:"url_ios"`
+	UrlAndroid  string `json:"url_android"`
+	UrlYYB      string `json:"url_yyb"`
+}
+
+func AppInfoView(w http.ResponseWriter, r *http.Request) {
+	accountID := "testaccount"
+	// checkSession(w, r)
+
+}
+
+// str1, _ := session.Values["Token"].(string)
+
+// 	fmt.Println(str1)
+// 	userFromToken, err := checkToken(str1)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+
 func AddAppView(w http.ResponseWriter, r *http.Request) {
+
 }
 
 func UserInfoView(w http.ResponseWriter, r *http.Request) {
